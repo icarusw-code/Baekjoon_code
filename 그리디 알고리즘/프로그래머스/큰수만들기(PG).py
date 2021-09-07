c820a123f7d914eae8c71aa5a738d7481cf9f68d
icarusw-code <77170611+icarusw-code@users.noreply.github.com>
2021-09-08 03:09:31 +0900

    greedy

---
+++ 그리디 알고리즘/프로그래머스/큰수만들기(PG).py
@@ -0,0 +1,10 @@
+def solution(number, k):
+    stack = []
+
+    for i in range(len(number)):
+        while stack and k > 0 and stack[-1] < number[i]:
+            stack.pop()
+            k -= 1
+        stack.append(number[i])
+    return "".join(stack[:len(stack)-k])
+    
